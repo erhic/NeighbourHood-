18272fe9c45e8ebe98e8f49859579202c89ae466
erhic <eric.gichovi@student.moringaschool.com>
2022-03-20 13:16:03 +0300

    added a form ,inherited from usercration form then added my custom fields

---
+++ neighapp/forms.py
@@ -0,0 +1,40 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm 
+
+from .models import Profile
+
+# a below created a class userregistration form and inherited UserCreationForm to overide it by adding fields and styling it#
+class UserRegisterForm(UserCreationForm):
+    
+    email=forms.EmailField()
+    
+    class Meta:
+        
+        model=User
+        fields=['username','email','password1','password2']
+ 
+ 
+# forms below will rely on model which inthis case is profile   
+# form below will rely on model which inthis case is profile   
+class UserUpdateForm(forms.ModelForm):
+    
+    email=forms.EmailField()
+    
+    class Meta:
+        
+        model=User
+        fields=['username','email','contact']
+    
+
+    
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    
+    class Meta:
+        
+        model=Profile
+        fields=['image']
+
+    
