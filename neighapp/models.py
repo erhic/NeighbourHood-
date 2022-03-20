07b0c7682ed2005505de3103c9b0bddd3e626d1e
erhic <eric.gichovi@student.moringaschool.com>
2022-03-20 13:11:53 +0300

    added a function to resize the image to 300 by 300

--- neighapp/models.py
+++ neighapp/models.py
@@ -16,4 +16,13 @@ class Profile(models.Model):
     def __str__(self):
         return f'{self.user.username}Profile'
     
-    
+    def save(self):
+        super().save()
+        #  the below variable will store  every instance of the image before risizing
+        img= Image.open(self.image.path)
+        
+        if img.height>300 or img.width>300:
+            output_size=(300,300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
+        
