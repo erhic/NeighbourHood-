596e84d684b4bf821960db1a2d9be26e010721ff
erhic <eric.gichovi@student.moringaschool.com>
2022-03-20 13:10:31 +0300

    created user profile model inhering user model

--- neighapp/models.py
+++ neighapp/models.py
@@ -1,3 +1,19 @@
+
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import User
+from  PIL import Image
+
+
+
+class Profile(models.Model):
+    user= models.OneToOneField(User, on_delete=models.CASCADE)
+    image= models.ImageField(default='default.png',upload_to='profile_pics')
+    contact=models.CharField(blank=True,max_length=50)
+    
+    
+    def __str__(self):
+        return f'{self.user.username}Profile'
+    
+    
