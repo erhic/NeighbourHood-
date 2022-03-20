5a3db71cc630d955dae9b7fd1a7c602fcb654b80
erhic <eric.gichovi@student.moringaschool.com>
2022-03-20 17:57:32 +0300

    added a setting to find media file using the url,for application to find the right path for images

--- neighapp/urls.py
+++ neighapp/urls.py
@@ -21,3 +21,5 @@ urlpatterns = [
     path('logout',auth_views.LogoutView.as_view(template_name='neighapp/logout.html'),name='logout')
     
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
