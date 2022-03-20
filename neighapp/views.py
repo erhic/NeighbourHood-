b7ca7298f877ad72a9a882b7b6c08d792ce60c8e
erhic <eric.gichovi@student.moringaschool.com>
2022-03-20 13:19:22 +0300

    added first view function,for registering user and rendering base.html,homepage

--- neighapp/views.py
+++ neighapp/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 # Create your views here.
+# from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from .forms import UserRegisterForm,UserUpdateForm,ProfileUpdateForm
+
+from django.contrib.auth.decorators import login_required
+
+
+def home(request):
+    return render(request,'neighapp/base.html')
+
+
+def register(request):
+    if request.method == 'POST':
+        # if form as method post and there is data posted return
+        form=UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username=form.cleaned_data.get('username')
+            messages.success(request,f'Account created for {username}!')
+            return redirect('home')
+    else:
+        
+        # else return a clean or empty form
+        form=UserRegisterForm()
+    
+    context={'form':form}
+    return render(request, 'neighapp/register.html',context)
