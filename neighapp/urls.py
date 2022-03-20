3e0b8fb8e132ba190cac1cfa825d6e5ef05e2785
erhic <eric.gichovi@student.moringaschool.com>
2022-03-20 13:46:19 +0300

    added the urls to login ,logout,register in the projects urls

--- neighapp/urls.py
+++ neighapp/urls.py
@@ -1,8 +1,24 @@
 from django.urls import path
 from . import views
+from django.contrib import admin
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from neighapp import views as user_views
+from neighapp import views 
+
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('admin/', admin.site.urls),
+    path('register/', user_views.register, name='register'),
+    path('home/', views.home, name='home'),
+    path('profileacc/',views.profile, name='profileacc'),
+   
+#    using  LoginView and LogoutView, class based views, they have the logic but we show them how to handle templates
+    path('login',auth_views.LoginView.as_view(template_name='neighapp/login.html'),name='login'),
+    path('logout',auth_views.LogoutView.as_view(template_name='neighapp/logout.html'),name='logout')
     
 ]
